7b2f5a59c7db528b239d94422805fa73b8d0cba3
BYSocket <qiangqiangli1993@gmail.com>
2015-09-09 17:35:39 +0800

    装饰模式-请求的工厂类

---
+++ src/org/javadp/decorator/servletReq/RequestFactory.java
@@ -0,0 +1,27 @@
+package org.javadp.decorator.servletReq;
+/*
+ * Copyright [2015] [Jeff Lee]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jeff Lee
+ * @since 2015-9-9 15:43:01
+ * 装饰模式-请求的工厂类
+ */
+public class RequestFactory {
+	public static Request getRequest() {
+		return new RequestImpl("Hello,BYSocket");
+	}
+}
